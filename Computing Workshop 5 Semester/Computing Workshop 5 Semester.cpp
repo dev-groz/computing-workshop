51a179524087218b58cee37de6681be39eb3c703
dev-groz <groza4ka@gmail.com>
2024-10-02 21:02:54 +0700

    Fixed order of x and t. And now correctly finding solution

--- Computing Workshop 5 Semester/Computing Workshop 5 Semester.cpp
+++ Computing Workshop 5 Semester/Computing Workshop 5 Semester.cpp
@@ -3,10 +3,11 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <fstream>
+#include <vector>
 
 #include <iomanip>
 
-const double tau = 0.05;
+const double tau = 0.1;
 const double h = 0.05;
 
 const double a = 0.28;
@@ -24,7 +25,7 @@ double phi(double x) {
 }
 
 double g1(double t) {
-    return 1.21 * t - sin(2 * M_PI * t) / 2;
+    return 1.21 * t * t - sin(2 * M_PI * t) / 2;
 }
 
 
@@ -35,8 +36,9 @@ int main()
     std::ofstream solution_file("solution.dat");
     solution_file << std::fixed << std::setprecision(10);
 
-    int x_len = (int)(1/tau);
-    int t_len = (int)(1/h);
+    const int t_len = (int)(1 / tau);
+    const int x_len = (int)(1 / h);
+
 
     double matrix[t_len][x_len] {0};
 
@@ -48,34 +50,31 @@ int main()
         }
     }
 
-    for (int n = 0; n < t_len; ++n){
-        matrix[n][0] = phi(h * n);
-    }
-
     for (int j = 0; j < x_len; ++j){
-        matrix[0][j] = g1(tau * j);
+        matrix[0][j] = phi(h * j);
+    }
+
+    for (int n = 1; n < t_len; ++n){
+        matrix[n][0] = g1(tau * n);
     }
 
     for (int n = 0; n < t_len - 1; ++n){
         for (int j = 1; j < x_len; ++j){
             double x_j = j * h;
             double t_n = n * tau;
+
             matrix[n + 1][j] = tau * (f(x_j, t_n) - a * (matrix[n][j] - matrix[n][j-1])/h) + matrix[n][j];
         }
     }
 
-    for (int n = 0; n < t_len - 1; ++n){
-        for (int j = 1; j < x_len; ++j){
-            double x_j = j * h;
-            double t_n = n * tau;
-            matrix[n + 1][j] = tau * (f(x_j, t_n) - a * (matrix[n][j] - matrix[n][j-1])/h) + matrix[n][j];
-        }
-    }
 
 
     for (int n = 0; n < t_len; ++n){
         for (int j = 0; j < x_len; ++j){
-            max_error = std::max(max_error, std::abs(U(j * h, n * tau) - matrix[n][j]));
+            double error = std::abs(U(j * h, n * tau) - matrix[n][j]);
+            if (error > max_error){
+                max_error = error;
+            }
             solution_file << j * h << "\t" << n * tau << "\t" << matrix[n][j] << "\n";
 
         }
